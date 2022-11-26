7091300125c76b2c7dc7579396026f3e0702dcb6
sz753404 <chengmingbo@gmail.com>
2022-11-26 13:28:54 +0100

    remove useless functions

--- hodgetraj/__init__.py
+++ hodgetraj/__init__.py
@@ -7,7 +7,7 @@ from .diffusionmap import diffusionMaps, affinity
 from .hodgedecomp import laplacian0,laplacian1
 from .hodgedecomp import triangle_list,gradop,divop,curlop,potential,grad,div,curl
 
-from .harmonic import harmonic_projection_matrix_with_w, truncated_delaunay, truncated_delaunay_keep_end, reset_edges, truncate_graph, untangle_starts_ends, untangle_starts_ends_with_orignal, connect_starts_ends_with_Delaunay, connect_starts_ends_with_Delaunay_
-from .util import norm01, tuple_increase, pairwise, top_n_from, is_in_2sets
+from .harmonic import harmonic_projection_matrix_with_w, truncated_delaunay, reset_edges, truncate_graph, connect_starts_ends_with_Delaunay
+from .util import norm01, tuple_increase, pairwise, top_n_from, is_in_2sets, kde_eastimate
 from .plotting import nxdraw_group_legend, plot_traj
 from .trajectory import trajectory_class, random_climb, random_climb_knn, distribute_traj
