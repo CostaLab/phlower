60f62113aba205ba07a82e2f2cfc07b3ac95d76b
sz753404 <chengmingbo@gmail.com>
2023-04-28 10:59:49 +0200

    stream fix save fig

--- phlower/external/stream.py
+++ phlower/external/stream.py
@@ -272,11 +272,11 @@ def plot_stream_sc(adata,root='S0',color=None,dist_scale=1,dist_pctl=95,preferen
             ax_i.set_title(title)
             plt.tight_layout(pad=pad, h_pad=h_pad, w_pad=w_pad)
             if(save_fig):
-                file_path_S = os.path.join(fig_path,root)
-                if(not os.path.exists(file_path_S)):
-                    os.makedirs(file_path_S)
+                #file_path_S = os.path.join(fig_path,root)
+                #if(not os.path.exists(file_path_S)):
+                #    os.makedirs(file_path_S)
                 #plt.savefig(os.path.join(file_path_S,'stream_sc_' + slugify(ann) + '.' + fig_format),pad_inches=1,bbox_inches='tight')
-                plt.savefig(os.path.join(file_path_S,'stream_sc_' + '.' + fig_format),pad_inches=1,bbox_inches='tight')
+                plt.savefig(os.path.join(fig_path,'stream_sc_' + '.' + fig_format),pad_inches=1,bbox_inches='tight')
                 plt.close(fig)
 
 
@@ -497,9 +497,9 @@ def plot_stream(adata,root='S0',color = None,preference=None,dist_scale=0.9,
         ax.set_title(title)
         plt.tight_layout(pad=pad, h_pad=h_pad, w_pad=w_pad)
         if(save_fig):
-            file_path_S = os.path.join(fig_path,root)
-            if(not os.path.exists(file_path_S)):
-                os.makedirs(file_path_S)
+            #file_path_S = os.path.join(fig_path,root)
+            #if(not os.path.exists(file_path_S)):
+            #    os.makedirs(file_path_S)
             #plt.savefig(os.path.join(file_path_S,'stream_' + slugify(ann) + '.' + fig_format),pad_inches=1,bbox_inches='tight')
-            plt.savefig(os.path.join(file_path_S,'stream_' + '.' + fig_format),pad_inches=1,bbox_inches='tight')
+            plt.savefig(os.path.join(fig_path,'stream_' + '.' + fig_format),pad_inches=1,bbox_inches='tight')
             plt.close(fig)
